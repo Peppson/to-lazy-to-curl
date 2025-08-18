4b6801848ee97efa7f94b02e982159c42ae9648d
Jesper W <jw_jw@hotmail.com>
2025-08-18 08:16:33 +0200

    fa icons

--- to-lazy-to-curl/MainWindow.xaml.cs
+++ to-lazy-to-curl/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         SetWindowSizeAndPosition();
-        UiService.MessageTextBlock = MessageTextBlock;
+        UiService.MessageTextBlock = MessageTextBlock;        
     }
 
     private void SetWindowSizeAndPosition()
